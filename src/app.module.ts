2c75f84e431473b8195b8b6fc23634f4743e1352
Vitor Manoel Santos Moura <v.program.py@gmail.com>
2024-01-21 22:32:35 -0300

    fix/file_structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
-import { ClasseController } from './modules/classes/controllers/classe.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [
-        ClasseController, AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
