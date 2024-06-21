533414aaa3b650de3623f87bb10578431951c713
Cauan Santos <cauansantos043@gmail.com>
2024-06-21 16:25:20 -0300

    feat(usecases): create login usecase

--- src/modules/user/infra/usecases/Login.usecase.ts
+++ src/modules/user/infra/usecases/Login.usecase.ts
@@ -1,25 +1,44 @@
 import * as bcrypt from 'bcrypt';
-
 import { Injectable } from '@nestjs/common';
 import { UseCaseInterface } from 'src/shared/domain/protocols/UseCase.protocol';
-import { LoginUseCaseDTO } from '../../domain/dtos/usecases/Login.usecase.dto';
+import {
+  LoginUseCaseDTO,
+  LoginUseCaseResponseDTO,
+} from '../../domain/dtos/usecases/Login.usecase.dto';
 import { UserRepository } from '../db/repositories/User.repository';
-import { Permissions } from '@prisma/client';
+import { NotFoundUserException } from '../../domain/errors/NotFoundUser.exception';
+import { InvalidPasswordException } from '../../domain/errors/InvalidPassword.exception';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class LoginUseCase implements UseCaseInterface {
-  constructor(private UserRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async execute({
     email,
     password,
-  }: LoginUseCaseDTO): Promise<LoginUseCaseDTO> {
-    const user = await this.UserRepository.findByEmail({ email });
+  }: LoginUseCaseDTO): Promise<LoginUseCaseResponseDTO> {
+    const user = await this.userRepository.findByEmail({ email });
 
     if (!user) {
+      throw new NotFoundUserException();
     }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      throw new InvalidPasswordException();
+    }
+
+    const payload = { email: user.email };
+    const accessToken = this.jwtService.sign(payload);
+
     return {
-      user,
+      user_id: user.id,
+      access_token: accessToken,
     };
   }
 }
